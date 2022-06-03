aeb4ad9843e84496b2aa30c881f7cf5f8f972634
Ayush <ayushprajapati2563@gmail.com>
2022-06-03 10:22:08 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,52 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import ResponsiveAppBar from './Component/Card';
 import RecipeReviewCard from './Component/Card/styles';
 
 function App() {
+  // const [countryList , setcountryList] = useState([]);
+  // useEffect(() => {
+    
+  //  localStorage.setItem('Token',12345);
+  //  const Token = localStorage.getItem('Token');
+  //   getCountry();
+    
+  // }, []);
+
+  // const getCountry = () => {
+  //   const url = 'https://restcountries.com/v3.1/all';
+  //   fetch(url, {
+  //     method: 'GET'
+  //   })
+  //     .then((x) => x.json())
+  //     .then((y) => {
+  //       console.log('ddhhdhd', y);
+  //       y.map((element) => {
+  //         console.log(element);
+  //         setcountryList(y);
+        
+  //       });
+  //     });
+  // };
+
   return (
     <>
-      <ResponsiveAppBar/> <br/>
+       <ResponsiveAppBar/> 
+       <br/>
       <RecipeReviewCard/>
+      <br/>
+      {/* <div>
+        {countryList.map((item,index)=>{
+          console.log(item);
+          return(
+            <div>
+              <p>
+                {item.region}
+              </p>
+            </div>
+          )
+        })}
+      </div> */} 
     </>
   );
 }
--- src/Component/Card/styles.js
+++ src/Component/Card/styles.js
@@ -14,9 +14,7 @@ import FavoriteIcon from '@mui/icons-material/Favorite';
 import ShareIcon from '@mui/icons-material/Share';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
-import Container from '@mui/material/Container';
-
-
+import Grid from '@mui/material/Grid';
 
 const ExpandMore = styled((props) => {
   const { expand, ...other } = props;
@@ -37,66 +35,81 @@ export default function RecipeReviewCard() {
   };
 
   return (
-    <div  style={{ width:'100vw', alignItems:'center', backgroundColor:'yellowgreen'}}>
-    <Container maxWidth="sm">
-    <Card sx={{ maxWidth: 345 }}>
-      <CardHeader
-        avatar={
-          <Avatar sx={{ bgcolor: red[500] }} aria-label="Comment">
-            A
-          </Avatar>
-        }
-        action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
-          </IconButton>
-        }
-        title="Ayush Prajapati"
-        subheader="June 25,2021"
-      />
-      <CardMedia
-        component="img"
-        height="194"
-        image="https://write.geeksforgeeks.org/static/media/Group%20210.08204759.svg"
-        alt="Ayush"
-      />
-      <CardContent>
-        <Typography variant="body2" color="text.secondary">
-          This impressive paella is a perfect party dish and a fun meal to cook
-          together with your guests. Add 1 cup of frozen peas along with the mussels,
-          if you like.
-        </Typography>
-      </CardContent>
-      <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
-        </IconButton>
-        <IconButton aria-label="share">
-          <ShareIcon />
-        </IconButton>
-        <ExpandMore
-          expand={expanded}
-          onClick={handleExpandClick}
-          aria-expanded={expanded}
-          aria-label="show more"
-        >
-          <ExpandMoreIcon />
-        </ExpandMore>
-      </CardActions>
-      <Collapse in={expanded} timeout="auto" unmountOnExit>
+    // <div  style={{ width:'100vw', alignItems:'center', backgroundColor:'yellowgreen'}}>
+    // <Container maxWidth="sm">
+    <Grid
+      container
+      spacing={0}
+      marginTop="0%"
+      flexDirection= "column"
+      display="flex"
+      alignItems="center"
+      //justifyContent="center"
+      style={{ minHeight: '100vh' }}
+      backgroundColor ='yellowgreen'
+    >
+    
+      <Card sx={{ maxWidth: 345 }}>
+        <CardHeader
+          avatar={
+            <Avatar sx={{ bgcolor: red[500] }} aria-label="Comment">
+              A
+            </Avatar>
+          }
+          action={
+            <IconButton aria-label="settings">
+              <MoreVertIcon />
+            </IconButton>
+          }
+          title="Ayush Prajapati"
+          subheader="June 25,2021"
+        />
+        <CardMedia
+          component="img"
+          height="194"
+          image="https://write.geeksforgeeks.org/static/media/Group%20210.08204759.svg"
+          alt="Ayush"
+        />
         <CardContent>
-          <Typography paragraph>Method:</Typography>
-          <Typography paragraph>
-            Heat 1/2 cup of the broth in a pot until simmering, add saffron and set
-            aside for 10 minutes.
-          </Typography>
-          <Typography>
-            Set aside off of the heat to let rest for 10 minutes, and then serve.
+          <Typography variant="body2" color="text.secondary">
+            This impressive paella is a perfect party dish and a fun meal to cook
+            together with your guests. Add 1 cup of frozen peas along with the mussels,
+            if you like.
           </Typography>
         </CardContent>
-      </Collapse>
-    </Card>
-  </Container>
-  </div>
+        <CardActions disableSpacing>
+          <IconButton aria-label="add to favorites">
+            <FavoriteIcon />
+          </IconButton>
+          <IconButton aria-label="share">
+            <ShareIcon />
+          </IconButton>
+          <ExpandMore
+            expand={expanded}
+            onClick={handleExpandClick}
+            aria-expanded={expanded}
+            aria-label="show more"
+          >
+            <ExpandMoreIcon />
+          </ExpandMore>
+        </CardActions>
+        <Collapse in={expanded} timeout="auto" unmountOnExit>
+          <CardContent>
+            <Typography paragraph>Method:</Typography>
+            <Typography paragraph>
+              Heat 1/2 cup of the broth in a pot until simmering, add saffron and set
+              aside for 10 minutes.
+            </Typography>
+            <Typography>
+              Set aside off of the heat to let rest for 10 minutes, and then serve.
+            </Typography>
+          </CardContent>
+        </Collapse>
+      </Card>
+
+      </Grid>
+    
+    //  </Container> 
+    // </div>
   );
 }
